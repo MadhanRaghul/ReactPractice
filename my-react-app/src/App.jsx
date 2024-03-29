6a3554877939549078ac5b7a4d3c2bb1f6d4958b
MadhanRaghul <madhanraghul2002@gmail.com>
2024-03-29 13:11:57 +0530

    'react'

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header"
+import Some from "./Some"
+
+function App() {
+    return(
+      <>
+      <Header/>
+      <Header/>
+      <Some/>
+      </>
+    )
+}
+
+export default App
